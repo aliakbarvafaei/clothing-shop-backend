8a23f8298772b3664ee985b7288c2677ce094661
aliakbarvafaei <72415266+aliakbarvafaei@users.noreply.github.com>
2022-09-25 13:37:50 +0330

    change for heroku five

--- index.js
+++ index.js
@@ -589,13 +589,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-if(process.env.NODE_ENV === "production" ){
+// if(process.env.NODE_ENV === "production" ){
   app.use(express.static('build'));
   app.get('*', (req, res)=>{
     req.sendFile(path.resolve(__dirname,'build','index.html'))
   })
-}
+//}
 
-app.listen(PORT, function() {
+app.listen(PORT, (err)=> {
+  if(err) return console.log(err);
   console.log(`Server started on port ${PORT}`);
 });
