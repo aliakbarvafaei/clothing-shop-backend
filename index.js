7b1840603b4520e707a64360ae21d534525eb81c
aliakbarvafaei <72415266+aliakbarvafaei@users.noreply.github.com>
2022-09-28 11:13:42 +0330

    change for deployment on cpanel

--- index.js
+++ index.js
@@ -231,7 +231,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/pub' + '/index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, function() {
   console.log(`Server started on port ${PORT}`);
